cfeb08df34513f82ae56128c3aed992dc615cb2a
hazim-j <hazim.jumali@gmail.com>
2023-11-12 13:57:20 +1100

    WIP: Test pm_getERC20TokenQuotes

--- scripts/barz/transfer.ts
+++ scripts/barz/transfer.ts
@@ -1,16 +1,13 @@
 import { ethers } from "ethers";
 import { Client, Presets } from "userop";
 import { CLIOpts } from "../../src";
+import { ERC20Quotes } from "../../src/getERC20quotes";
 // @ts-ignore
 import config from "../../config.json";
 
 export default async function main(t: string, amt: string, opts: CLIOpts) {
-  const paymasterMiddleware = opts.withPM
-    ? Presets.Middleware.verifyingPaymaster(
-        config.paymaster.rpcUrl,
-        config.paymaster.context
-      )
-    : undefined;
+  const quotes = new ERC20Quotes(config.paymaster.rpcUrl);
+  const paymasterMiddleware = opts.withPM ? quotes.middleware : undefined;
   const BarzAccount = await Presets.Builder.Barz.init(
     new Presets.Signers.BarzSecp256r1(config.secp256r1Key),
     config.rpcUrl,
@@ -22,13 +19,33 @@ export default async function main(t: string, amt: string, opts: CLIOpts) {
 
   const target = ethers.utils.getAddress(t);
   const value = ethers.utils.parseEther(amt);
-  const res = await client.sendUserOperation(
-    BarzAccount.execute(target, value, "0x"),
-    {
-      dryRun: opts.dryRun,
-      onBuild: (op) => console.log("Signed UserOperation:", op),
-    }
-  );
+  quotes
+    .setTokens([
+      {
+        address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+        approve: ethers.utils.parseUnits("1", 6),
+      },
+      {
+        address: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
+        approve: ethers.utils.parseUnits("0.001", 18),
+      },
+      {
+        address: "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6",
+        approve: ethers.utils.parseUnits("0.0001", 8),
+      },
+    ])
+    .setPreferredToken("0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619")
+    .setCallDataFn((address, approveData) => {
+      return BarzAccount.executeBatch(
+        [address, target],
+        [ethers.constants.Zero, value],
+        [approveData, "0x"]
+      ).getCallData();
+    });
+  const res = await client.sendUserOperation(BarzAccount, {
+    dryRun: opts.dryRun,
+    onBuild: (op) => console.log("Signed UserOperation:", op),
+  });
   console.log(`UserOpHash: ${res.userOpHash}`);
 
   console.log("Waiting for transaction...");
---
+++ src/getERC20quotes.ts
@@ -0,0 +1,108 @@
+import { BigNumberish, Bytes, BytesLike, ethers } from "ethers";
+import { UserOperationMiddlewareCtx, Utils, Constants } from "userop";
+import { ERC20_ABI } from "./abi";
+
+interface Token {
+  address: string;
+  approve: BigNumberish;
+}
+
+interface QuoteResponse {
+  symbol: string;
+  decimals: number;
+  etherTokenExchangeRate: BigNumberish;
+  serviceFeePercent: BigNumberish;
+}
+
+interface UserOperationOverridesResponse {
+  paymasterAndData: BytesLike;
+  preVerificationGas: BytesLike;
+  verificationGasLimit: BytesLike;
+  callGasLimit: BytesLike;
+}
+
+interface TokenQuotes {
+  token: string;
+  userOperationOverrides: UserOperationOverridesResponse;
+  quote: QuoteResponse;
+}
+
+interface GetERC20TokenQuotesResponse {
+  postOpGas: BigNumberish;
+  etherUSDExchangeRate: BigNumberish;
+  tokens: Array<TokenQuotes>;
+}
+
+export class ERC20Quotes {
+  private callDataFn: (address: string, approveData: BytesLike) => BytesLike;
+  private tokens: Array<Token>;
+  private pref: string;
+  private provider: ethers.providers.JsonRpcProvider;
+
+  private approveCallData = async (spender: string, token: Token) => {
+    const erc20 = new ethers.Contract(token.address, ERC20_ABI);
+    return erc20.interface.encodeFunctionData("approve", [
+      spender,
+      token.approve,
+    ]);
+  };
+
+  constructor(paymasterRpc: string) {
+    this.provider = new ethers.providers.JsonRpcProvider(paymasterRpc);
+    this.callDataFn = () => "0x";
+    this.tokens = [];
+    this.pref = "";
+  }
+
+  setTokens = (tokens: Array<Token>): ERC20Quotes => {
+    this.tokens = tokens;
+    return this;
+  };
+
+  setPreferredToken = (token: string): ERC20Quotes => {
+    this.pref = token;
+    return this;
+  };
+
+  setCallDataFn = (
+    fn: (address: string, approveData: BytesLike) => BytesLike
+  ): ERC20Quotes => {
+    this.callDataFn = fn;
+    return this;
+  };
+
+  middleware = async (ctx: UserOperationMiddlewareCtx) => {
+    const accounts = (await this.provider.send("pm_accounts", [
+      Constants.ERC4337.EntryPoint,
+    ])) as Array<string>;
+
+    const quotesReq = await Promise.all(
+      this.tokens.map(async (token) => ({
+        token: token.address,
+        callData: this.callDataFn(
+          token.address,
+          await this.approveCallData(accounts[0], token)
+        ),
+      }))
+    );
+
+    const res = (await this.provider.send("pm_getERC20TokenQuotes", [
+      Utils.OpToJSON(ctx.op),
+      ctx.entryPoint,
+      quotesReq,
+    ])) as GetERC20TokenQuotesResponse;
+
+    console.log(JSON.stringify(res, null, 2));
+    const i = res.tokens.findIndex(
+      (tokenQuote) => tokenQuote.token === this.pref
+    );
+    if (i === -1) {
+      throw new Error("Could not find preferred token.");
+    }
+    ctx.op.callData = quotesReq[i].callData;
+    ctx.op = {
+      ...ctx.op,
+      ...res.tokens[i].userOperationOverrides,
+    };
+  };
+}
